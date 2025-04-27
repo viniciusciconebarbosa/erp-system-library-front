a8ac920f773db0797495e022a4365d6319578803
vinicius <viniciuscicone@gmail.com>
2025-04-26 22:35:48 -0300

    adding a test user

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,8 +24,43 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-muted/50 to-muted p-4">
-      <LoginForm />
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        {/* Div com as informações de teste */}
+        
+
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Bem-vindo de volta
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Entre com suas credenciais para acessar sua conta
+          </p>
+        </div>
+
+        <LoginForm />
+        <div className="bg-muted/50 border rounded-lg p-2 shadow-sm">
+          <h3 className="text-sm font-medium text-muted-foreground mb-1 pb-2 border-b">
+            Credenciais para teste:
+          </h3>
+          <div className="space-y-2 text-sm">
+            <div className="flex items-center">
+              <span className="font-medium min-w-[4rem]">Email:</span>
+              <code className="bg-muted px-2 py-0.5 rounded text-muted-foreground ml-2">
+                test@email.com
+              </code>
+            </div>
+            <div className="flex items-center">
+              <span className="font-medium min-w-[4rem]">Senha:</span>
+              <code className="bg-muted px-2 py-0.5 rounded text-muted-foreground ml-2">
+                Vi12345678
+              </code>
+            </div>   
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
+
