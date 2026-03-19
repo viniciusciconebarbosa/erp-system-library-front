4fa63298cb3dc1ebbf61105095759d676c4f5468
Vinicius <viniciuscicone@gmail.com>
2026-03-19 12:10:47 -0300

    fixing layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={inter.className + " overflow-y-hidden"}>
         <Providers>{children}</Providers>
       </body>
     </html>
